1a36788e368c2c715c8d3008e8697a12a93f3b36
Eastyn <1282861514@qq.com>
2026-04-30 22:27:50 +0800

    漏了一个文件

---
+++ src/views/user/NewRecruitmentView.vue
@@ -0,0 +1,7 @@
+<template>
+  <RecruitmentApplicationForm mode="new_student" />
+</template>
+
+<script setup lang="ts">
+import RecruitmentApplicationForm from '@/components/user/RecruitmentApplicationForm.vue'
+</script>
